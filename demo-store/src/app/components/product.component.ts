6abe869616e5649a46011494a2dfb27d2096766b
Felipe Armijo <luis.armijo@mayor.cl>
2021-09-21 14:21:50 -0300

    input output

--- demo-store/src/app/components/product.component.ts
+++ demo-store/src/app/components/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { Product } from '../product.model';
 
@@ -6,13 +6,14 @@ import { Product } from '../product.model';
     selector: 'app-product',
     templateUrl: './product.component.html'
 })
+
 export class ProductComponent {
 
-    product: Product = {
-        id: '1',
-        image: 'assets/images/camiseta.png',
-        title: 'Camiseta',
-        price: 80000,
-        description: 'bla bla bla bla bla'
+    @Input() product: Product;
+    @Output() productClicked: EventEmitter <any> = new EventEmitter();
+
+    addCart(){
+        console.log('añadir al carrito');
+        this.productClicked.emit(this.product.id);
     }
 }
