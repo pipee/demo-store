cd6e73ff3f0c2d6e36d0dc3f5795862ca7e9a563
Felipe Armijo <luis.armijo@mayor.cl>
2021-09-21 02:14:06 -0300

    ngFor iteración items

--- demo-store/src/app/app.component.ts
+++ demo-store/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo-store';
+
+  items = ['luis', 'felipe', 'antonio'];
+
+  addItem(){
+    this.items.push('nuevo item');
+  }
+  deleteItem(index: number){
+    this.items.splice(index, 1);
+  }
 }
