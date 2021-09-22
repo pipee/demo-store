34f0efc6c9ece10714d54a87ea351e71817c2d0b
Felipe Armijo <luis.armijo@mayor.cl>
2021-09-22 01:17:28 -0300

    creación de service y creación de ruta con parametro id product-detail

---
+++ demo-store/src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductService
+  ) { }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      const id = params.id;
+      console.log(id);
+      const product = this.productsService.getProduct(id);
+      console.log(product);
+    });
+  }
+
+}
