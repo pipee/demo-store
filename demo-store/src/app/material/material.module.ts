ae0612831fff2419280b5419314785d16f7dc12e
Felipe Armijo <luis.armijo@mayor.cl>
2021-09-23 13:12:32 -0300

    creación de header y footer

--- demo-store/src/app/material/material.module.ts
+++ demo-store/src/app/material/material.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 
 
 
@@ -9,10 +12,16 @@ import {MatButtonModule} from '@angular/material/button';
   declarations: [],
   imports: [
     CommonModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule
   ],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule
   ]
 })
 export class MaterialModule { }
--- demo-store/src/app/shared/shared.module.ts
+++ demo-store/src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { HighlightDirective } from './directives/highlight/highlight.directive';
 import { HeaderComponent } from './components/header/header.component'; 
 import { FooterComponent } from './components/footer/footer.component'; 
 
+import { MaterialModule } from './../material/material.module';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MaterialModule
   ],
   exports: [
     HeaderComponent,
