ac942917e2537856fbf20fe45c1c0f43544aed68
Felipe Armijo <luis.armijo@mayor.cl>
2021-09-23 23:47:54 -0300

    componente de creacion de producto, uso de formbuider y validaciones de datos

--- demo-store/src/app/admin/admin-routing.module.ts
+++ demo-store/src/app/admin/admin-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FormProductComponent } from './components/form-product/form-product.component';
 import { NavComponent } from './components/nav/nav.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
@@ -26,6 +27,10 @@ const routes: Routes = [
       {
         path: 'products',
         component: ProductsListComponent
+      },
+      {
+        path: 'products/create',
+        component: FormProductComponent
       }
     ]
   }
