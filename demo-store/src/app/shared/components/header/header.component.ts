ee872fa86de1ae594a076e53d77e0290b4f2cd20
Felipe Armijo <luis.armijo@mayor.cl>
2021-09-24 01:41:54 -0300

    cambio de logica para no utilizar subscribe, tambien se utiliza pipe map para encontrar total

--- demo-store/src/app/shared/components/header/header.component.ts
+++ demo-store/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { map } from 'rxjs/operators';
 
 import { CartService } from './../../../core/services/cart.service';
 
@@ -9,15 +12,15 @@ import { CartService } from './../../../core/services/cart.service';
 })
 export class HeaderComponent implements OnInit {
 
-  total = 0;
+  total$: Observable<number>;
 
   constructor(
     private cartService: CartService
   ) { 
-    this.cartService.cart$.subscribe(products => {
-      console.log(products);
-      this.total = products.length;
-    });
+    this.total$ = this.cartService.cart$
+    .pipe(
+      map(products => products.length)
+    );
   }
 
   ngOnInit(): void {
